2e928f1791a993d8b6e8c9a959349bf1261c53ad
Alejandro Prieto <samuel.prieto-r@mail.escuelaing.edu.co>
2026-04-20 20:26:32 -0500

    Agregando todo

---
+++ src/main/java/com/eci/arcn/producer/config/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package com.eci.arcn.producer.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${app.rabbitmq.queue}")
+    private String queueName;
+
+    @Value("${app.rabbitmq.exchange}")
+    private String exchangeName;
+
+    @Value("${app.rabbitmq.routingkey}")
+    private String routingKey;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+}
